9d239fc9d5154d2d46fa8e66dbcf34b1c111967c
YESWANTH0221 <yeswanthac21@gmail.com>
2022-11-20 13:21:16 +0530

    HONOUR 8C

---
+++ src/org/kumar/KundraiKumar.java
@@ -0,0 +1,33 @@
+package org.kumar;
+
+public class KundraiKumar {
+	public static void main(String[] args) {
+byte age=24;
+System.out.println("person age is"+age);
+
+short carnum=1180;
+System.out.println("person carnum is+carnum");
+
+int pincode=600069;
+System.out.println("person pincode is+pincode");
+
+long phonenum=636994826;
+System.out.println("person phonenum is+phonenum");
+
+char grade='D';
+System.out.println("person grade is +grade");
+
+String name="kumar";
+System.out.println("person name is+name");
+
+boolean married=true;
+System.out.println("person married is+married");
+float height =172;
+System.out.println("person height is+height");
+double shares=12345.9877;
+System.out.println("person shares is+sahres");
+
+
+
+	}
+}
---
+++ src/org/kumar/Yeswa5.java
@@ -0,0 +1,17 @@
+package org.kumar;
+
+
+public class Yeswa5 {
+	
+	public void petrolprice() {
+		
+		System.out.println("petrol price is 100");
+		
+	}
+     public void dieselprice() {
+		
+		System.out.println("petrol price is 90");
+		
+	
+     }	
+}
